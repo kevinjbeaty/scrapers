231a9bb9ce8c24c2d27c0a839b4cb8fee31f27e8
Kevin Beaty <kevinjbeaty@gmail.com>
2024-02-21 20:01:50 -0700

    Update jobscraper.py

--- jobscraper.py
+++ jobscraper.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import json
+SLACKURL = os.environ['SLACKURL']
 url = 'https://www.journalismjobs.com/job-listings?keywords=&location=0&jobType=&date=&industries=0%2C1%2C5%2C3%2C4%2C13%2C7%2C8%2C14%2C12%2C6%2C10%2C15%2C11&position=&diversity=&focuses=&salary=&company=&title=&virtual=&page=1&count=25'
 data = requests.get(url).text
 soup = BeautifulSoup(data, 'html.parser')
@@ -13,4 +14,4 @@ for i in listings:
         title = i.find('h3',{'class':'job-item-title'}).text
         location = i.find_all('i')[0].parent.text.replace(' ','').replace('\n','')
         postThis = {'text':'<https://example.com|' + company + ' -- ' + title + ' -- ' + location + '>'} 
-        response = requests.post(webhookurl, data=json.dumps(postThis), headers={'Content-Type': 'application/json'})        
+        response = requests.post(SLACKURL, data=json.dumps(postThis), headers={'Content-Type': 'application/json'})        
